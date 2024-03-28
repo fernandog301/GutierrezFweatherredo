a44c94b07d21a6154ffd37882206fb3af3c2343f
fernandog301 <fernandog301g@gmail.com>
2024-03-28 14:04:51 -0700

    test

--- src/app/DataService/GetWeatherApi.tsx
+++ src/app/DataService/GetWeatherApi.tsx
@@ -1,16 +1,18 @@
 import { Interface } from "readline";
+
 export const ApiKey = 'f86e3df09941ea6ed9ee289a745f4331';
 
 export const fetchData = async (lat:string , long:string ) =>{
-const promise = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${ApiKey}&units=imperial`);
+const promise = await fetch(
+`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${ApiKey}&units=imperial`);
 const data: Interface = await promise.json();
 return data;
 };
 
 
-
 export const FetchGeoLocation = async (searchInput: string) => {
-    const promise = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${ApiKey}&units=imperial`)
+    const promise = await fetch(`
+    https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${ApiKey}&units=imperial`)
     const data: Interface = await promise.json();
     return data;
 };
